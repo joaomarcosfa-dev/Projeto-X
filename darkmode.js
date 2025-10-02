5949cd57b41259883d846a6e95069c8773d4caa6
João Marcos Ferreira de Almeida <joaomarcosferreiradealmeida74@gmai.com>
2025-10-02 01:38:01 -0400

    adicionando o darkmode

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.documentElement.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.documentElement.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null)
+}
+
+if (darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
